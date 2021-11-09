944efaef1d6501c2ff2c738d4e322e33052ee33f
Jorge <jorgelizgo@gmail.com>
2021-11-09 23:51:55 +0100

    monocularDepth.py

--- point_cloud/images/stereoDepth.py
+++ point_cloud/images/stereoDepth.py
@@ -1,8 +1,8 @@
 import os
 import airsim
 import numpy as np
-
-
+from matplotlib import pyplot as plt
+import cv2 as cv
 
 client = airsim.VehicleClient()
 
@@ -23,3 +23,13 @@ img_rgb_right = left.reshape(responses[1].height, responses[1].width, 3)
 # write to png 
 airsim.write_png(os.path.normpath("leftimage" + '.png'), img_rgb_left) 
 airsim.write_png(os.path.normpath("rightimage" + '.png'), img_rgb_right) 
+
+left_image = cv.imread("leftimage.png", cv.IMREAD_GRAYSCALE)
+right_image = cv.imread("rightimage.png", cv.IMREAD_GRAYSCALE)
+
+stereo = cv.StereoBM_create(numDisparities=16, blockSize=15)
+depth = stereo.compute(left_image, right_image)
+
+plt.imshow(depth)
+plt.axis("off")
+plt.show()
