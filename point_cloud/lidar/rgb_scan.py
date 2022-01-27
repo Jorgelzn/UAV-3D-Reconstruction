75c4038e39db1717865ce264b6416048df16793e
Jorge <jorgelizgo@gmail.com>
2022-01-27 21:44:14 +0100

    proyection trial 0.1

--- point_cloud/lidar/rgb_scan.py
+++ point_cloud/lidar/rgb_scan.py
@@ -53,40 +53,74 @@ class Lidar:
     def execute(self,vehicle_name,lidar_names):
         print('Scanning Has Started\n')
         existing_data_cleared = False   #change to true to superimpose new scans onto existing .asc files
+
+        #CAMERA CONSTANTS
+
+        img_width = 672
+        center_x = img_width/2
+        img_height = 376
+        center_y = img_height/2
+        img_fov = 90
+        ratio = img_width/img_height
+
+        epsilon = .001
+
+        s_x = ( img_width - epsilon ) / 2
+        d_x = ( img_width - epsilon ) / 2
+
+        s_y = ( img_height - epsilon ) / (-2*ratio)
+        d_y = ( img_height - epsilon ) / 2
+
+
+
+        # Compute the focal length
+        fov_rad = img_fov * np.pi/180
+        focal = np.tan(fov_rad/2.0)
+        
         try:
             while True:
                 responses = self.client.simGetImages([airsim.ImageRequest("front", airsim.ImageType.Scene, False, False)])
+
+                #CAMERA VALUES
+                camera_x = responses[0].camera_position.x_val
+                camera_y = responses[0].camera_position.y_val
+                camera_z = responses[0].camera_position.z_val
+                camera_orientation_w = responses[0].camera_orientation.w_val
+                camera_orientation_x = responses[0].camera_orientation.x_val
+                camera_orientation_y = responses[0].camera_orientation.y_val
+                camera_orientation_z = responses[0].camera_orientation.z_val
+
+                #IMAGE
+
                 photo = np.fromstring(responses[0].image_data_uint8, dtype=np.uint8)
                 img = photo.reshape(responses[0].height, responses[0].width, 3)
                 img = img[...,::-1]   #brg to rgb
-                #img=np.flipud(img)     #flip img
-                #imgplot = plt.imshow(img)
-                #plt.show()
+
+
                 for lidar_name in lidar_names:
                     filename = f"{vehicle_name}_{lidar_name}_pointcloud_rgb.asc"
                     if not existing_data_cleared:
                         f = open(filename,'w')
                     else:
                         f = open(filename,'a')
+
                     lidar_data = self.client.getLidarData(lidar_name=lidar_name,vehicle_name=vehicle_name)
                     points = len(lidar_data.point_cloud)/3
                     cloud = len(lidar_data.point_cloud)
-                    print("points obtained: ",points)
+                    
                     for i in range(0, cloud, 3):
-                        pixel = int(i/3)
-                        if pixel < 400:
-                            point = (cloud-3)-i
-                            xyz = lidar_data.point_cloud[point:point+3]
-                            print("pixel:",pixel)
-                            r = img[pixel][350][0]
-                            g = img[pixel][350][1]
-                            b = img[pixel][350][2]
-                            #print(r,g,b)
-                            rgb = int('%02x%02x%02x' % (r, g, b), 16)
-                            f.write("%f %f %f %f\n" % (xyz[0],xyz[1],xyz[2],rgb))
-                        else:
-                            break
-                            
+                        xyz = lidar_data.point_cloud[i:i+3]
+
+                        #PROYECCION
+                        X_NDC = xyz[0]/(ratio*xyz[2]*focal)
+                        Y_NDC = xyz[1]/(xyz[2]*focal)
+                        pixel_x = int(s_x * X_NDC + d_x)
+                        pixel_y = int(s_y * Y_NDC + d_y)
+                        r,g,b = [color for color in img[pixel_y,pixel_x]]
+
+                        rgb = int('%02x%02x%02x' % (r,g,b), 16)
+                        f.write("%f %f %f %f\n" % (xyz[0],xyz[1],xyz[2],rgb))
+
                     f.close()
                 existing_data_cleared = True
         except KeyboardInterrupt:
--- point_cloud/read_pcd.py
+++ point_cloud/read_pcd.py
@@ -17,17 +17,11 @@ if __name__ == "__main__":
     sensor = 1
     file = "C:/Users/"+getpass.getuser()+"/Documents/Airsim/"+"Drone1_LidarSensor"+str(sensor)+"_pointcloud.pcd"
     file2 = "C:/Users/"+getpass.getuser()+"/Documents/Airsim/"+"Drone1_LidarSensor"+str(sensor)+"_pointcloud_rgb.pcd"
-    pcd = read(file)
+    pcd = read(file2)
     #plane_model, inliers = pcd.segment_plane(distance_threshold=0.01, ransac_n=3, num_iterations=1000)
     #inlier_cloud = pcd.select_by_index(inliers)
     #outlier_cloud = pcd.select_by_index(inliers, invert=True)
     #inlier_cloud.paint_uniform_color([1, 0, 0])
     #outlier_cloud.paint_uniform_color([0.6, 0.6, 0.6])
     #o3d.visualization.draw_geometries([inlier_cloud, outlier_cloud])
-    o3d.visualization.draw_geometries([pcd])
-    alpha = 0.01
-    print(f"alpha={alpha:.3f}")
-    mesh = o3d.geometry.TriangleMesh.create_from_point_cloud_alpha_shape(pcd, alpha)
-    mesh.compute_vertex_normals()
-    o3d.visualization.draw_geometries([mesh], mesh_show_back_face=True)
 
