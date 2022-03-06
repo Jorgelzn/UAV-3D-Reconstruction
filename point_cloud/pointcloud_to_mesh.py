c71be791f9fedd4846c80fcfd602968e56c3f74a
Jorge <jorgelizgo@gmail.com>
2022-03-06 15:19:48 +0100

    car scan

--- point_cloud/pointcloud_to_mesh.py
+++ point_cloud/pointcloud_to_mesh.py
@@ -8,25 +8,28 @@ def read(file):
     print("Loading point cloud")
     pcd = o3d.io.read_point_cloud(file)
     print(pcd)
+    pcd_center = pcd.get_center()
+    pcd = pcd.crop(o3d.geometry.AxisAlignedBoundingBox(np.array(pcd_center)-10,np.array(pcd_center)+10))
+    print("central point:",pcd_center)
     o3d.visualization.draw_geometries([pcd],width=1400,height=900)
-
     return pcd
 
 
 def alpha_shape(pcd,alpha):
-    print("Calculating mesh with Alpha Shape")
-    mesh = o3d.geometry.TriangleMesh.create_from_point_cloud_alpha_shape(pcd,alpha)
+    print("Calculating mesh with Alpha Shape alpha:",alpha)
+    mesh = o3d.geometry.TriangleMesh.create_from_point_cloud_alpha_shape(pcd, alpha)
+    mesh.compute_vertex_normals()
 
     return mesh
 
 def ball_pivoting(pcd,radii):
-    print("Calculating mesh with Ball Pivoting")
+    print("Calculating mesh with Ball Pivoting radius:",radii)
     mesh = o3d.geometry.TriangleMesh.create_from_point_cloud_ball_pivoting(pcd, o3d.utility.DoubleVector(radii))
 
     return mesh
 
 def poisson_surface(pcd,depth):
-    print('Calculating mesh Poisson Surface Reconstruction')
+    print("Calculating mesh Poisson Surface Reconstruction depth:",depth)
     with o3d.utility.VerbosityContextManager(o3d.utility.VerbosityLevel.Debug) as cm:
         mesh, densities = o3d.geometry.TriangleMesh.create_from_point_cloud_poisson(pcd,depth)
 
@@ -43,27 +46,27 @@ if __name__ == "__main__":
     object_file = "C:/Users/"+getpass.getuser()+"/Documents/Airsim/object.ply"
 
     pcd = read(pcd_file)
-    
+
 
     #pointcloud segmentation
-    plane_model, inliers = pcd.segment_plane(distance_threshold=0.3, ransac_n=3, num_iterations=2000)
+    plane_model, inliers = pcd.segment_plane(distance_threshold=0.05, ransac_n=6, num_iterations=4000)
     inlier_cloud = pcd.select_by_index(inliers)
     outlier_cloud = pcd.select_by_index(inliers, invert=True)
 
     o3d.visualization.draw_geometries([pcd.select_by_index(inliers).paint_uniform_color([1, 0, 0]), pcd.select_by_index(inliers, invert=True).paint_uniform_color([0.6, 0.6, 0.6])],width=1400,height=900)
     
-
+    pcd = outlier_cloud
+    print("object points:",pcd)
     #normals estimation
-    outlier_cloud.estimate_normals(search_param=o3d.geometry.KDTreeSearchParamHybrid(radius=0.1, max_nn=30))
-
-    o3d.visualization.draw_geometries([outlier_cloud],point_show_normal=True,width=1400,height=900)
-
+    pcd.estimate_normals(search_param=o3d.geometry.KDTreeSearchParamHybrid(radius=1.5, max_nn=20000))
+    pcd.orient_normals_to_align_with_direction(orientation_reference=np.array([0., 0., -1.]))
+    o3d.visualization.draw_geometries([pcd],point_show_normal=True,width=1400,height=900)
 
     #mesh conversion
-    #mesh = alpha_shape(outlier_cloud,0.2)
-    #mesh = ball_pivoting(outlier_cloud,[0.1, 0.2, 0.2, 0.4])
-    mesh = poisson_surface(outlier_cloud,9)
-    mesh.vertex_colors = o3d.utility.Vector3dVector(np.clip(np.asarray(mesh.vertex_colors), 0, 1))
+    #mesh = alpha_shape(pcd,0.5)
+    mesh = ball_pivoting(pcd,np.arange(0.05, 2.0, 0.05))
+    #mesh = poisson_surface(pcd,10)
+    #mesh.vertex_colors = o3d.utility.Vector3dVector(np.clip(np.asarray(mesh.vertex_colors), 0, 1))
 
     o3d.visualization.draw_geometries([mesh],width=1400,height=800)
 
